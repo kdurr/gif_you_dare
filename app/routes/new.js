969af9b2091e1f0858541b660980e43cada278bb
kdurr <kristina.l.durr@gmail.com>
2016-07-03 10:54:12 -0400

    Working state

--- app/routes/new.js
+++ app/routes/new.js
@@ -1,13 +1,55 @@
 import Ember from 'ember';
 
-let gifs = [{
-  id: 1,
-  title: 'Panda cuties',
-  image: 'http://i.imgur.com/QbNmpiY.gif'
-}];
-
 export default Ember.Route.extend({
-  model() {
-    return gifs;
+
+  model: function(params) {
+      let url = "https://www.reddit.com/r/gifs/top.json?sort=new&limit=100";
+      var redditId = params.subreddit_id;
+      return Ember.$.getJSON(url).then(function(res) {
+        return res.data.children;
+      });
+  },
+
+  upperCase: function(str) {
+    return str.String().toUpperCase();
   }
+
 });
+
+// export default Ember.Route.extend({
+//   model() {
+//     let url = "https://www.reddit.com/r/gifs/top.json?sort=new&limit=100",
+//         gifsPromise = Ember.$.getJSON(url),
+//         gifList = {};
+//
+//     gifsPromise.then(function(obj) {
+//       let objList = obj.data.children
+//       for(var i = 0; i < objList.length; i++) {
+//         let dataSet = objList[i].data
+//         gifList[dataSet.title] = dataSet.url
+//       }
+//
+//       console.log(gifList);
+//       return gifList;
+//     });
+//   }
+// });
+
+// export default Ember.Route.extend({
+//   model() {
+//     let url = "https://www.reddit.com/r/gifs/top.json?sort=new&limit=100",
+//         gifsPromise = Ember.$.getJSON(url);
+//
+//     gifsPromise.then(function(obj) {
+//       let objList = obj.data.children,
+//           Gif = Ember.Object.extend({
+//             title: this.title,
+//             image: this.url
+//           });
+//
+//       return objList.map(function(d) {
+//         return Gif.create(d.data);
+//       });
+//     });
+//   }
+// });
