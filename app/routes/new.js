dc09e253cc1114c9b28e02f7ab7c1cd4f1cb8bcf
kdurr <kristina.l.durr@gmail.com>
2016-07-07 21:19:30 -0400

    Remove unnecessary code

--- app/routes/new.js
+++ app/routes/new.js
@@ -8,35 +8,13 @@ export default Ember.Route.extend({
       return Ember.$.getJSON(url).then(function(res) {
         return res.data.children;
       });
-  },
-
-  upperCase: function(str) {
-    return str.String().toUpperCase();
   }
 
 });
-
-// export default Ember.Route.extend({
-//   model() {
-//     let url = "https://www.reddit.com/r/gifs/top.json?sort=new&limit=100",
-//         gifsPromise = Ember.$.getJSON(url),
-//         gifList = {};
 //
-//     gifsPromise.then(function(obj) {
-//       let objList = obj.data.children
-//       for(var i = 0; i < objList.length; i++) {
-//         let dataSet = objList[i].data
-//         gifList[dataSet.title] = dataSet.url
-//       }
-//
-//       console.log(gifList);
-//       return gifList;
-//     });
-//   }
-// });
-
 // export default Ember.Route.extend({
-//   model() {
+//
+//   model: function() {
 //     let url = "https://www.reddit.com/r/gifs/top.json?sort=new&limit=100",
 //         gifsPromise = Ember.$.getJSON(url);
 //
@@ -52,4 +30,5 @@ export default Ember.Route.extend({
 //       });
 //     });
 //   }
+
 // });
