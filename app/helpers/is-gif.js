407ce3a97cb105a640e339ce15b5cd631dadb852
Kristina Durr <kristina.l.durr@gmail.com>
2016-12-26 00:24:27 -0500

    Clean up isGif helper

--- app/helpers/is-gif.js
+++ app/helpers/is-gif.js
@@ -3,18 +3,16 @@ import Ember from 'ember';
 export function isGif(params) {
   if (/\.gif$/.test(params[0])) {
     return Ember.String.htmlSafe(`<img class='gif' src=${params[0]}>`);
-  } else if (/\.gifv$/.test(params[0])) {
-    return Ember.String.htmlSafe(`<video class='gif'><source src='${params[0]}' type='video/webm'></video>`);
-  } else {
-    if (/gfycat/.test(params[0])) {
-      let gfyRegexp = /(?:gfycat\.com\/)([a-zA-Z0-9]+)/,
-          gfyCatKey = gfyRegexp.exec(params[0])[1],
-          gfyCatUrl = 'http://giant.gfycat.com/' + gfyCatKey + '.gif'
+  } else if (/\.mp4$/.test(params[0])) {
+    return Ember.String.htmlSafe(`<video autoplay loop class='embeded-video gif'><source src=${params[0]} type='video/mp4'></video>`);
+  } else if (/gfycat/.test(params[0])) {
+    let gfyRegexp = /(?:gfycat\.com\/)([a-zA-Z0-9]+)/,
+        gfyCatKey = gfyRegexp.exec(params[0])[1],
+        gfyCatUrl = 'http://giant.gfycat.com/' + gfyCatKey + '.gif'
 
-      return Ember.String.htmlSafe(`<img class='gif' src=${gfyCatUrl}>`);
-    } else {
-      return Ember.String.htmlSafe(`<video autoplay loop class='embeded-video gif'><source src=${params[0]} type='video/mp4'></video>`);
-    }
+    return Ember.String.htmlSafe(`<img class='gif' src=${gfyCatUrl}>`);
+  } else {
+    return Ember.String.htmlSafe(`<div class='gif'><h1>Sorry, nothing found :(</h1></div>`)
   }
 }
 
