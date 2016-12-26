c996870c05d72975456d6c1c8d4d1b3a6c7c0a46
Kristina Durr <kristina.l.durr@gmail.com>
2016-12-25 22:27:09 -0500

    Hack to get gfycat gif

--- app/helpers/is-gif.js
+++ app/helpers/is-gif.js
@@ -3,23 +3,18 @@ import Ember from 'ember';
 export function isGif(params) {
   if (/\.gif$/.test(params[0])) {
     return Ember.String.htmlSafe(`<img class='gif' src=${params[0]}>`);
+    // return Ember.String.htmlSafe(`<img class='gif' src=${params[0]} onload='displayGif(this)' style='display: none;'>`);
   } else {
     if (/gfycat/.test(params[0])) {
-      // return Ember.String.htmlSafe(`<iframe class='imgur-embed' width='100%' height='325' frameborder='1' src=${params[0]}#embed></iframe>`);
-      return Ember.String.htmlSafe(`<div style='position:relative;padding-bottom:calc(100% / 1.85)'><iframe src=${params[0]} frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe></div>`);
+      let gfyRegexp = /(?:gfycat\.com\/)([a-zA-Z0-9]+)/,
+          gfyCatKey = gfyRegexp.exec(params[0])[1],
+          gfyCatUrl = 'http://giant.gfycat.com/' + gfyCatKey + '.gif'
+
+      return Ember.String.htmlSafe(`<img class='gif' src=${gfyCatUrl}>`);
     } else {
       return Ember.String.htmlSafe(`<video autoplay loop class="embeded-video"><source src=${params[0]} type="video/mp4"></video>`);
     }
   }
 }
-
-
-// <div style='position:relative;padding-bottom:calc(100% / 1.85)'>
-//   <iframe src="{{gif.data.url}}" frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen>
-//   </iframe>
-// </div>
-
-
-// <video autoplay loop poster="placeholder.jpg" id="backgroundgif"><source src=${params[0]} type="video/mp4"></video>
 
 export default Ember.Helper.helper(isGif);
