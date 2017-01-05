3cc76bd57f64cab4db2007e5e6982a0094055d2e
Kristina Durr <kristina.l.durr@gmail.com>
2017-01-04 20:58:32 -0500

    Fix sidebar transition

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,13 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     toggle: function(direction) {
-      $('.ui.sidebar').sidebar('toggle');
+      $('.ui.sidebar')
+        .sidebar('setting', 'transition', 'push')
+        .sidebar('toggle');
     },
     closeSidebar: function() {
-      this.send('refreshGifs');
       $('.list-item').on('click', function() {
         $('.ui.sidebar').sidebar('toggle');
       });
+      this.send('refreshGifs');
     }
   }
 });
