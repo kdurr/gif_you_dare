ecc7452b26df3b5666e48573bf2622ca3cdd14a2
Kristina Durr <kristina.l.durr@gmail.com>
2017-01-04 22:03:00 -0500

    Not dry, but fix sidebar transition on link click

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,15 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    toggle: function(direction) {
+    toggle: function() {
       $('.ui.sidebar')
         .sidebar('setting', 'transition', 'push')
         .sidebar('toggle');
     },
     closeSidebar: function() {
-      $('.list-item').on('click', function() {
-        $('.ui.sidebar').sidebar('toggle');
-      });
+      $('.ui.sidebar')
+        .sidebar('setting', 'transition', 'push')
+        .sidebar('toggle');
       this.send('refreshGifs');
     }
   }
