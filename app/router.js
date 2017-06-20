f620cbf3552c52a3740a384b7b7e8efee678488e
Kristina Durr <kristina.l.durr@gmail.com>
2017-06-19 23:49:28 -0400

    Add google analytics

--- app/router.js
+++ app/router.js
@@ -9,4 +9,13 @@ Router.map(function() {
   this.route('gif', { path: 'r/:subreddit' });
 });
 
+Router.reopen({
+  notifyGoogleAnalytics: function() {
+    return ga('send', 'pageview', {
+        'page': this.get('url'),
+        'title': this.get('url')
+      });
+  }.on('didTransition')
+});
+
 export default Router;
