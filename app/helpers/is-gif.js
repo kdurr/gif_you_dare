89d3012f1cb1edbfc0abf88efe9dfca21f0fc2fd
Kristina Durr <kristina.l.durr@gmail.com>
2017-10-14 19:56:21 -0400

    Hack to get gfycat showing

--- app/helpers/is-gif.js
+++ app/helpers/is-gif.js
@@ -7,12 +7,17 @@ export function isGif(params) {
     return Ember.String.htmlSafe(`<video autoplay loop class='embeded-video gif'><source src=${params[0]} type='video/mp4'></video>`);
   } else if (/gfycat/.test(params[0])) {
     let gfyRegexp = /(?:gfycat\.com\/)([a-zA-Z0-9]+)/,
-        gfyCatKey = gfyRegexp.exec(params[0])[1],
-        gfyCatUrl = 'http://giant.gfycat.com/' + gfyCatKey + '.gif';
+        gfyCatKey = gfyRegexp.exec(params[0])[1];
 
-    return Ember.String.htmlSafe(`<img class='gif' src=${gfyCatUrl}>`);
+    return Ember.String.htmlSafe(`
+      <div>
+        <div style='position:relative;padding-bottom:57%''>
+          <iframe src='https://gfycat.com/ifr/${gfyCatKey}' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe>
+          <a href='#' class='gif' style="position:absolute; top:0; left:0; display:inline-block; width:100%; height:100%; z-index:5;"></a>
+        </div>
+      </div>`);
   } else {
-    return Ember.String.htmlSafe(`<div class='gif'><h1>Sorry, nothing found :(</h1></div>`);
+    return Ember.String.htmlSafe(`<div class='gif'><h1 class='no-gif'>Sorry, this one isn't supported yet!</h1></div>`);
   }
 }
 
